b51502b491d7d25aafb399d73dc9a090b33d3512
Joe DePloy <deploy_joe@bentley.edu>
2023-04-05 12:38:39 -0400

    april 5

---
+++ app/src/main/java/com/example/cs480_project/AddingExpenses.java
@@ -0,0 +1,122 @@
+package com.example.cs480_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class AddingExpenses extends AppCompatActivity {
+
+    private EditText editAmount, editDesc;
+    private Spinner chooseType;
+    private Button addReceipt, saveButton,cancelButton;
+    private ImageButton chooseDate;
+    private TextView dateTextView;
+    private DatePickerDialog datePickerDialog;
+
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_expenses);
+
+        // get references to edit text views
+        editAmount = (EditText) findViewById(R.id.editAmount);
+        editDesc = (EditText) findViewById(R.id.editDesc);
+        dateTextView = findViewById(R.id.dateDisplay);
+
+        // references to spinners
+        chooseType = (Spinner) findViewById(R.id.chooseType);
+        
+        // get references to buttons
+        addReceipt = (Button) findViewById(R.id.addReceipt);
+        saveButton = (Button) findViewById(R.id.save_button);
+        cancelButton = (Button) findViewById(R.id.cancel_button);
+        chooseDate = (ImageButton) findViewById(R.id.chooseDateButton);
+
+        //initialize calendar and date format
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+
+        // Create a DatePickerDialog with the current date as the default
+        chooseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddingExpenses.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, monthOfYear);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                        String dateString = dateFormat.format(calendar.getTime());
+                        chooseDate.setContentDescription(dateString);
+
+                        dateTextView.setText(dateFormat.format(calendar.getTime()));
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        // Set up the spinner adapter for expense types
+        ArrayAdapter<CharSequence> adapterTypes = ArrayAdapter.createFromResource(
+                this, R.array.expense_types, android.R.layout.simple_spinner_item);
+        adapterTypes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        chooseType.setAdapter(adapterTypes);
+
+
+        //set button to cancel activity
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+        //set button to save expense
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // finish later
+
+            }
+        });
+
+        //set button to allow user to upload their receipt
+        addReceipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // finish later
+            }
+        });
+
+    }
+    
+}
--- app/src/main/java/com/example/cs480_project/MainActivity.java
+++ app/src/main/java/com/example/cs480_project/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.cs480_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.AbstractCollection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //test
-        
+
+        //get references to button (test)
+        // get reference to the button
+        addExpense = (Button) findViewById(R.id.addExpense);
+
+        // set up click listener for the button
+        addExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddingExpenses.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
