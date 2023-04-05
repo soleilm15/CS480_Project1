1aacd0bbf413f71d9f5327e4220829ce7482418c
Joe DePloy <deploy_joe@bentley.edu>
2023-04-05 12:50:18 -0400

    april 5

---
+++ app/src/main/java/com/example/cs480_project/AddingBudget.java
@@ -0,0 +1,18 @@
+package com.example.cs480_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingBudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_budget);
+
+
+
+
+    }
+}
